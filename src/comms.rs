dca869ca7cbf889d475942fd62f52306a0495a11
K.Karas <konradkar2@gmail.com>
2025-05-30 00:35:41 +0200

    add prefix len framing

--- src/comms.rs
+++ src/comms.rs
@@ -1,4 +1,5 @@
 use std::{
+    array,
     io::{self, Read, Write},
     net::TcpStream,
 };
@@ -27,6 +28,8 @@ pub enum ClientError {
     Unknown(String),
 }
 
+const BUFF_SIZE: usize = 4096;
+
 impl Comms {
     pub fn connect(&mut self, server_ip: &str) -> Result<(), io::Error> {
         let stream = TcpStream::connect(server_ip).inspect_err(|err| {
@@ -53,48 +56,72 @@ impl Comms {
     }
 
     pub fn receive_message(&mut self) -> Result<Message, CommError> {
-        let mut buffer = [0; 4096];
+        let mut prefix_buffer: [u8; 4] = [0; 4];
+        self.read_into_buff(prefix_buffer.as_mut_slice())?;
 
-        let bytes = self
-            .connection
-            .as_ref()
-            .unwrap()
-            .read(&mut buffer)
-            .map_err(|err| {
-                if err.kind() == io::ErrorKind::WouldBlock {
-                    CommError::WouldBlock
-                } else {
-                    eprintln!("[ERROR] read failed {}", err);
-                    CommError::Unknown
-                }
-            })?;
+        let frame_len = u32::from_be_bytes(prefix_buffer);
 
-        if bytes == 0 {
-            eprintln!("[ERROR] lost connection");
-            Err(CommError::Disconnected)?
+        let mut message_buffer = vec![0u8; frame_len as usize];
+        let bytes = self.read_into_buff(message_buffer.as_mut_slice())?;
+
+        if bytes != frame_len as usize {
+            eprintln!(
+                "Received different sized than expected in prefix: expected: {}, actual: {}",
+                frame_len, bytes
+            );
+            return Err(CommError::Unknown);
         }
 
         let (decoded, _): (Message, usize) =
-            bincode::decode_from_slice(&buffer[..bytes], self.bincode_cfg).map_err(|err| {
-                eprintln!("[ERROR] Failed to deserialize data (read size: {}): {}", bytes, err);
-                CommError::Unknown
-            })?;
+            bincode::decode_from_slice(message_buffer.as_slice(), self.bincode_cfg).map_err(
+                |err| {
+                    eprintln!(
+                        "[ERROR] Failed to deserialize data (read size: {}): {}",
+                        bytes, err
+                    );
+                    CommError::Unknown
+                },
+            )?;
 
         Ok(decoded)
     }
 
-    pub fn send_message(&mut self, message: &Message) -> Result<(), CommError> {
-        let encoded: Vec<u8> =
-            bincode::encode_to_vec(message, self.bincode_cfg).map_err(|err| {
-                eprintln!("[ERROR] failed to serialize data: {}", err);
-                CommError::InvalidData
-            })?;
+    fn read_into_buff(&mut self, dst: &mut [u8]) -> Result<usize, CommError> {
+        let bytes = self.connection.as_ref().unwrap().read(dst).map_err(|err| {
+            if err.kind() == io::ErrorKind::WouldBlock {
+                CommError::WouldBlock
+            } else {
+                eprintln!("[ERROR] read failed {}", err);
+                CommError::Unknown
+            }
+        })?;
 
-        let bytes_sent = self
-            .connection
+        if bytes > 0 {
+            Ok(bytes)
+        } else {
+            Err(CommError::Disconnected)
+        }
+    }
+
+    pub fn send_message(&mut self, message: &Message) -> Result<(), CommError> {
+        let mut buffer: [u8; BUFF_SIZE] = [0; BUFF_SIZE];
+
+        let serialize_len =
+            bincode::encode_into_slice(message, &mut buffer[4..BUFF_SIZE], self.bincode_cfg).map_err(
+                |err| {
+                    eprintln!("[ERROR] failed to serialize data: {}", err);
+                    CommError::InvalidData
+                },
+            )? as u32;
+
+        buffer[0..4].copy_from_slice(&serialize_len.to_be_bytes());
+
+        let total_len: usize = serialize_len as usize + 4;
+
+        self.connection
             .as_ref()
             .unwrap()
-            .write(&encoded)
+            .write_all(&buffer[..total_len])
             .map_err(|err| {
                 if err.kind() == io::ErrorKind::WouldBlock {
                     CommError::WouldBlock
@@ -104,12 +131,7 @@ impl Comms {
                 }
             })?;
 
-        if bytes_sent == 0 {
-            eprintln!("[ERROR] lost connection");
-            Err(CommError::Disconnected)?
-        }
-
-        println!("written bytes: {}", bytes_sent);
+        println!("written bytes: {}", total_len);
 
         Ok(())
     }
